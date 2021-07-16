693af614697020afaab3aadd37e5940643964da8
MichalWes <massive7m@gmail.com>
2021-07-16 12:52:22 +0200

    Index created

--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect, render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect, response
+from django.urls import reverse
 
 monthly_challenges = {
     "january": "Ahoj Muminku",
@@ -19,17 +20,29 @@ monthly_challenges = {
 # Create your views here.
 
 
+def index(request):
+    list_items = ""
+    for month in monthly_challenges.keys():
+        month_path = reverse("month-challenge", args=[month])
+        print(month_path)
+        list_items += f"<li><a href=\"{month_path}\">{month.capitalize()}</a></li>"
+    response_data = f"<ul>{list_items}</ul>"
+    return HttpResponse(response_data)
+
+
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
     if month > len(months):
-        return HttpResponseNotFound("Ahoj Inny zwierzaczku!")
-    redirect = months[month - 1]
-    return HttpResponseRedirect("/challenges/" + redirect)
+        return HttpResponseNotFound("<h1>Ahoj Inny zwierzaczku!<h1>")
+    redirect_month = months[month - 1]
+    redirect_path = reverse("month-challenge", args=[redirect_month])
+    return HttpResponseRedirect(redirect_path)
 
 
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
+        response_data = f"<h1>{challenge_text}<h1>"
     except:
-        return HttpResponseNotFound("Ahoj Inny zwierzaczku!")
-    return HttpResponse(challenge_text)
+        return HttpResponseNotFound("<h1>Ahoj Inny zwierzaczku!<h1>")
+    return HttpResponse(response_data)
