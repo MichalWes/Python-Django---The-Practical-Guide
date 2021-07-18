cda54927f3606e8d19dbe487a4f72ba4b9e8a3dc
MichalWes <massive7m@gmail.com>
2021-07-18 23:00:45 +0200

    Tags

--- challenges/views.py
+++ challenges/views.py
@@ -22,12 +22,13 @@ monthly_challenges = {
 
 def index(request):
     list_items = ""
-    for month in monthly_challenges.keys():
-        month_path = reverse("month-challenge", args=[month])
-        print(month_path)
-        list_items += f"<li><a href=\"{month_path}\">{month.capitalize()}</a></li>"
-    response_data = f"<ul>{list_items}</ul>"
-    return HttpResponse(response_data)
+    months = monthly_challenges.keys()
+
+    return render(request, "challenges/index.html", {
+        "months": months
+    })
+
+    
 
 
 def monthly_challenge_by_number(request, month):
