bc858ae1756b63ea767efb3a821b0c9a97fa2d67
MichalWes <massive7m@gmail.com>
2021-08-11 21:03:01 +0200

    Admin

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,3 +1,13 @@
+from book_outlet.views import book_detail
 from django.contrib import admin
 
+from .models import Book
+
 # Register your models here.
+
+
+class BookAdmin(admin.ModelAdmin):
+    readonly_fields = ("slug")
+
+
+admin.site.register(Book, BookAdmin)
